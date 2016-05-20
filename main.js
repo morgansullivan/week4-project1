3df4aa9af11203e3a4601aa95e304da506fb115c
morgansullivan <morgan.siobhan0@gmail.com>
2016-05-19 20:34:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Add your JS here.
+
+$(document).ready(function(){
+ 
+     $(".hideMe").click(function(){
+        $(this).hide();
+   
+  });
+  
+    $(".changeColor").click(function(){
+        $("li:nth-child(3)").css("color", "#8A2BE2");
+  });
+  
+      $("li:nth-child(4)").click(function(){
+       $("li:nth-child(4)").append("<li>I am an extra list item</li>");
+  });
+  
+});
