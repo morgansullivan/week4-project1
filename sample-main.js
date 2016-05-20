d053a621982067fbb5ccf6a6e6509d3c05edfd3d
morgansullivan <morgan.siobhan0@gmail.com>
2016-05-19 22:14:21 -0700

    Update sample-main.js

--- sample-main.js
+++ sample-main.js
@@ -3,35 +3,40 @@
 // We'll be using the html in index.html for the following jQuery exercises.
 // After you've completed an exercise, comment out your code (unless otherwise indicated).
 
-$(document).ready(function(){
+//$(document).ready(function(){
 
 // 1. Hide all of the paragraphs on the index.html page.
 
-$("p").hide();
+//$("p").hide();
 
 
 // 2. Hide all of the h2's on the page.
-$("h2").hide();
+    //$("h2").hide();
 
 
 // 3. Hide all of the elements in the document.
 
- $("html").hide();
+    // $("html").hide();
 
 
 // 4. Hide all links on the page
- $("a").hide();
+
+    // $("a").hide();
 
 
 // 5. Hide all of the odd table rows (Austin, Colin, & Jessica's rows)
 
+     //$(document).ready(function(i){
+        // $("table tr:odd").hide();
+  //});
+
 
 // 6. Hide all paragraphs when they're clicked.
   
-     $("p").click(function(){
-        $(this).hide();
+     //$("p").click(function(){
+       // $(this).hide();
    
-  });
+  //});
 
 
 
